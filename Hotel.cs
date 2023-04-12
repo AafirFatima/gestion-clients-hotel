bac01e78fae6772840470009014a79482a397110
AafirFatima <2708749@collegelacite.ca>
2023-04-12 00:25:50 -0400

    Message de commit

---
+++ Hotel.cs
@@ -0,0 +1,18 @@
+﻿
+using System.Collections.Generic;
+
+public class Hotel
+{
+    public List<Client> Clients { get; set; }
+
+    public Hotel()
+    {
+        Clients = new List<Client>();
+    }
+
+    public void AjouterClient(Client client)
+    {
+        Clients.Add(client);
+    }
+}
+
---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Hotel hotel = new Hotel();
+
+        Client client1 = new Client("Doe", "John", "john.doe@email.com", "1234 5678 9012 3456", 101);
+        hotel.AjouterClient(client1);
+
+        Client client2 = new Client("Smith", "Jane", "jane.smith@email.com", "2345 6789 0123 4567", 102);
+        hotel.AjouterClient(client2);
+
+        foreach (Client client in hotel.Clients)
+        {
+            Console.WriteLine($"Nom : {client.Nom}, Prénom : {client.Prenom}, Courriel : {client.Courriel}, Carte de crédit : {client.CarteCredit}, Numéro de chambre : {client.NumeroChambre}");
+        }
+    }
+}
+
